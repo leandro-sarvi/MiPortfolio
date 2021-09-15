ee6023d8599d61ce3d66a2e7393a1e1b11a42aca
Leandro.sarvi <leandromartinsarvi@hotmail.com>
2021-09-15 17:06:58 -0300

    asd

--- js/title-animado.js
+++ js/title-animado.js
@@ -10,16 +10,6 @@ function barcodeAnimation()
       span.toggleClass('highlighted');
      //span.fadeToggle("slow");
     }, 200*i);});
-  
-  barcodeSpans.each(function(i)
-  {
-    let span = $(this);
-    setTimeout(function()
-    {
-      // remove the highlighting from each span with 20ms between each
-      span.toggleClass('highlighted');
-      
-    }, 20*i);});
 }
 
 $(document).ready(function()
